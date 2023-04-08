ed63fedc3eceb79898e1df4667ad8fd1da583a32
sean <sean@ubuntu-22.04.02-LTS.myguest.virtualbox.org>
2023-04-08 16:47:58 +0300

    Code with TODOs and first check

--- hw1/CBC-HMAC.py
+++ hw1/CBC-HMAC.py
@@ -51,7 +51,17 @@ class AEAD_AES_128_CBC_HMAC_SHA_256(AEAD):
         :param data: input data
         :return: stripped data
         """
-        ?
+        pad_len = data[-1]
+        actual_data, padding = data[:-(pad_len + 1)], data[-(pad_len + 1): -1]
+        for byte in padding: # TODO verify we check all
+            if byte != pad_len:
+                raise "Padding is not valid" #TODO verify what to do in this case
+        expected_pad_len = self.block_len - ((len(actual_data) + 1) % self.block_len) #TODO maybe extract, also used in __pad
+        if expected_pad_len != pad_len:
+            print(f"{expected_pad_len=} {pad_len=}")
+            raise "Padding is not valid" #TODO verify what to do in this case
+        return actual_data
+
 
     def __pad(self, data):
         """
@@ -59,7 +69,12 @@ class AEAD_AES_128_CBC_HMAC_SHA_256(AEAD):
         :param data: input data
         :return: padded data with length an integral multiple of block_len
         """
-        ?
+        pad_len = self.block_len - ((len(data) + 1) % self.block_len)
+        print(f"{pad_len=}, {data=}")
+        padding = pad_len.to_bytes(1, 'little') * (pad_len + 1)
+        return data + padding
+
+
 
     def __auth(self, data):
         """
@@ -85,6 +100,14 @@ class AEAD_AES_128_CBC_HMAC_SHA_256(AEAD):
         decryptor = cipher.decryptor()
         return decryptor.update(c) + decryptor.finalize()
 
+    # TODO, verify need to truncate, we added this function
+    def __auth_with_truncate(self, data):
+        """
+        Returning truncated auth byte string.
+        """
+        tag = self.__auth(data)
+        return tag[:self.mac_len]
+
     def authenticated_enc(self, data, aad, nonce):
         """
         Authenticated encryption
@@ -93,8 +116,10 @@ class AEAD_AES_128_CBC_HMAC_SHA_256(AEAD):
         :param nonce: nonce
         :return: c, the result of authenticated encryption
         """
-        ?
-
+        tag = self.__auth_with_truncate(aad + data)
+        print(f"{len(tag)=}")
+        padded_data = self.__pad(data + tag)
+        return self.__encrypt(padded_data, nonce)
 
     def authenticated_dec(self, c, aad, nonce):
         """
@@ -104,7 +129,16 @@ class AEAD_AES_128_CBC_HMAC_SHA_256(AEAD):
         :param nonce: nonce
         :return: decrypted data if verification succeeds, fail state else.
         """
-        ?
+
+        plain = self.__decrypt(c, nonce)
+        plain_no_pad = self.__strip_padding(plain)
+        data, tag = plain_no_pad[:-self.mac_len], plain_no_pad[-self.mac_len:]
+        expected_tag = self.__auth_with_truncate(aad + data)
+        if tag != expected_tag:
+            print(f"{data=} {tag=} {expected_tag=}")
+            print(f"{len(tag)=} {len(expected_tag)=}")
+            raise "Invalid tag" #TODO check what is the FAIL state in the doc
+        return data
 
 
 if __name__ == "__main__":
