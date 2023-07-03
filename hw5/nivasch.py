4e051e105a7d0264fd41317d448db8dc5d2ac03d
Adi Dinerstein <adi.dinerstein@gmail.com>
2023-07-03 19:45:23 +0300

    Implement nivasch

--- hw5/nivasch.py
+++ hw5/nivasch.py
@@ -5,6 +5,27 @@ An algorithm for cycle detection
 from prf import PRF
 
 
+class MonotonicStack:
+    def __init__(self):
+        self.stack = []
+
+    def top(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1]
+
+    def push(self, a):
+        if len(self.stack) == 0:
+            self.stack.append(a)
+            return None
+
+        while self.stack and self.stack[-1] > a:
+            self.stack.pop()
+        top_before_a = self.top()
+        self.stack.append(a)
+        return top_before_a
+
+
 def find_cycle(f, k, start):
     """
     Return a point x, where x is the first point detected by the Nivasch algorithm
@@ -13,7 +34,13 @@ def find_cycle(f, k, start):
     :param start: starting point for the algorithm
     :return: x, where x is a point inside of a cycle
     """
-    ?
+    stacks = [MonotonicStack() for _ in range(k)]
+    x = start
+    while True:
+        current_stack = stacks[x % k]
+        if current_stack.push(x) == x:
+            return x
+        x = f.calc(x)
 
 
 def main():
