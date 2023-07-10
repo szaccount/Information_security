d172d719a38453eb37cb5d447e4fd65d52415860
Adi Dinerstein <adi.dinerstein@gmail.com>
2023-07-11 00:18:35 +0300

    Save multiple start points

--- hw5/hellman_tables.py
+++ hw5/hellman_tables.py
@@ -29,21 +29,6 @@ class ModifiedPRF(object):
         """
         The function adjusts to the larger output if needed.
         """
-        # if domain < rang:
-        #     return self.f.calc(x) & (domain - 1)
-        #     # return self.f.calc(x & (domain - 1))
-        # elif domain > rang:
-        #     len_domain = self.f.domain_bytes * 8
-        #     len_rang = self.f.rang_bytes * 8
-        #     num_flavours = len_domain / len_rang
-        #     rv = 0
-        #     for i in range(num_flavours):
-        #         rv <<= len_rang
-        #         rv += self.f.calc((x + i) % domain)
-        #     return rv
-        # else:
-        #     return self.f.calc(x)
-        
         # Assume x is the size of rang.
         if domain < rang:
             # print(f"Got {x=}, {(domain - 1)=}, {(x & (domain - 1))=}")
@@ -69,7 +54,6 @@ class ModifiedPRF(object):
         domain = self.f.domain
         rang = self.f.rang
 
-        # return x
         if domain < rang:
             return x & (domain - 1)
         elif domain > rang:
@@ -93,30 +77,11 @@ def hellman_preprocess(m, t, f_tag):
     :param f_tag: oracle for a random function
     :return: a list of tables, where each table is a dictionary from the end points to the start points
     """
-    # tables = []
-    # print("In hellman_preprocess")
-    # for i in range(t):
-    #     print(f"flavour {i=}")
-    #     f_tag_i = lambda x: f_tag.calc((x+i) % f_tag.f.domain)
-    #     table = defaultdict(list)
-    #     for j in range(m):
-    #         # print(f"Wroking on chain {j=}")
-    #         # start_point = int.from_bytes(urandom(f_tag.f.domain_bytes), byteorder='big')
-    #         start_point = j % f_tag.f.domain
-    #         point = start_point
-    #         for _ in range(t):
-    #             point = f_tag_i(point)
-    #         table[point] = start_point
-    
-    #     tables.append(table)
-    #     print(i)
-    # return tables
-
     tables = []
     print("In hellman_preprocess")
     for i in range(t):
-        print(f"flavour {i=}")
-        f_tag_i = lambda x: f_tag.calc((x+i) % f_tag.f.rang)
+        # print(f"flavour {i=}")
+        f_tag_i = lambda x: f_tag.calc((x + i) % f_tag.f.rang)
         table = defaultdict(list)
         for j in range(m):
             # print(f"Wroking on chain {j=}")
@@ -126,8 +91,8 @@ def hellman_preprocess(m, t, f_tag):
             for _ in range(t):
                 point = f_tag_i(point)
             # if point in table:
-                # print(f"Got collision in endpoint {point}, tried putting {start_point=} but has {table[point]=}")
-            table[point] = start_point
+            #     print(f"Got collision in endpoint {point}, tried putting {start_point=} but has {table[point]=}")
+            table[point].append(start_point)
     
         tables.append(table)
         print(i)
@@ -143,70 +108,29 @@ def hellman_online(tables, t, y, f_tag):
     :param f_tag: modified oracle for a random function
     :return: x such that f(x)=y if the attack succeeded, else None
     """
-    # domain = f_tag.f.domain
-    # rang = f_tag.f.rang
-    
-    # y_tag = y
-
-    # print(f"In hellman_online {domain=} {rang=} {t=} {y=}")
-    # if domain < rang:
-    #     y_tag = y & (domain - 1)
-    # elif domain > rang:
-    #     diff_num_bytes = f_tag.f.domain_bytes - f_tag.f.rang_bytes
-    #     rand_suffix = int.from_bytes(urandom(diff_num_bytes), byteorder='big')
-    #     y_tag << (diff_num_bytes * 8)
-    #     y_tag += rand_suffix
-
-    # print(f"{y_tag}")
-
-    # points = [y_tag for _ in range(t)] # t as number of tables
-
-    # for step in range(t): # t as max number of steps
-    #     print(f"In {step=}")
-    #     for i in range(t): # t as number of tables
-    #         print(f"Working on table {i=}")
-    #         f_tag_i = lambda x: f_tag.calc((x + i) % f_tag.f.domain)
-    #         points[i] = f_tag_i(points[i])
-    #         points_i = points[i]
-    #         if points_i in tables[i].keys(): # TODO can also do if _ in tables[i], might be faster
-    #             print(f"{points_i} is endpoint")
-    #             ptr = tables[i][points_i] # init to start point
-    #             # !!!!!!!!!!!! tha problem we have here is that y not necessarily in chain if dimain != range
-    #             # also it might not be in chain if more than one chain leads to endpoint but we have one start
-    #             counter = 0
-    #             while counter <= t and f_tag_i(ptr) != y_tag: # TODO maybe need to verify against f_i and not f_tag_i also might be infinite
-    #                 # print(f"interation {counter} of searching for origin")
-    #                 counter += 1
-    #                 ptr = f_tag_i(ptr)
-    #             print(f"Found origin")
-    #             return (ptr + i) % f_tag.f.domain # flavour correction
-    # print("Didn't find origin")
-    # return None
-
     domain = f_tag.f.domain
     rang = f_tag.f.rang
     
-    points = [y for _ in range(t)] # t as number of tables
+    points_per_table = [y for _ in range(t)] # t as number of tables
 
     for step in range(t): # t as max number of steps
-        print(f"In {step=}")
+        # print(f"In {step=}")
         for i in range(t): # t as number of tables
-            print(f"Working on table {i=}")
+            # print(f"Working on table {i=}")
+            table = tables[i]
             f_tag_i = lambda x: f_tag.calc((x + i) % f_tag.f.rang)
-            points[i] = f_tag_i(points[i])
-            points_i = points[i]
-            if points_i in tables[i].keys(): # TODO can also do if _ in tables[i], might be faster
-                print(f"{points_i} is endpoint")
-                ptr = tables[i][points_i] # init to start point
-                # !!!!!!!!!!!! tha problem we have here is that y not necessarily in chain if dimain != range
-                # also it might not be in chain if more than one chain leads to endpoint but we have one start
-                counter = 0
-                while counter <= t and f_tag_i(ptr) != y: # TODO maybe need to verify against f_i and not f_tag_i also might be infinite
-                    # print(f"interation {counter} of searching for origin")
-                    counter += 1
-                    ptr = f_tag_i(ptr)
-                print(f"Found origin")
-                return (ptr + i) % f_tag.f.rang # flavour correction
+            current_point = f_tag_i(points_per_table[i])
+            points_per_table[i] = current_point
+            if current_point in table:
+                # print(f"{current_point} is endpoint")
+                for start_point in table[current_point]:
+                    ptr = start_point
+                    for _ in range(t):
+                        if f_tag_i(ptr) == y:
+                            # print(f"Found origin")
+                            return (ptr + i) % f_tag.f.rang # flavour correction
+                        ptr = f_tag_i(ptr)
+                   
     print("Didn't find origin")
     return None
 
@@ -248,10 +172,10 @@ def test_1():
     # The case where domain = range
     key = b'j\xb1\xd5\xfa\x92\x11X\x12\x00\xde3\xae\x16L8['
     block_size = 3
-    # m = 2 ** 8
-    # t = 2 ** 8
-    m = 2 ** 14
-    t = 2 ** 5
+    m = 2 ** 8
+    t = 2 ** 8
+    # m = 2 ** 14
+    # t = 2 ** 5
 
     f = PRF(key, block_size)
     return run_hellman(f, m, t)
@@ -288,9 +212,9 @@ def test_3():
 
 
 def main():
-    # print("Test 1 success rate:", test_1())
+    print("Test 1 success rate:", test_1())
     print("Test 2 success rate:", test_2())
-    # print("Test 3 success rate:", test_3())
+    print("Test 3 success rate:", test_3())
 
 
 if __name__ == "__main__":
