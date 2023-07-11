6c612179636cc185d73b7b8ad45bc346d8e0042b
Adi Dinerstein <adi.dinerstein@gmail.com>
2023-07-12 00:36:03 +0300

    More fixes

--- hw5/hellman_tables.py
+++ hw5/hellman_tables.py
@@ -15,7 +15,6 @@ class ModifiedPRF(object):
         """
         self.f = f
 
-    # !!!!!!!!!!!!!!!!!! said maybe need to adjust input rather than output
     def calc(self, x):
         """
         Calculate a modified f
@@ -26,12 +25,8 @@ class ModifiedPRF(object):
         domain = self.f.domain
         rang = self.f.rang
         
-        """
-        The function adjusts to the larger output if needed.
-        """
         # Assume x is the size of rang.
         if domain < rang:
-            # print(f"Got {x=}, {(domain - 1)=}, {(x & (domain - 1))=}")
             return self.f.calc(x & (domain - 1))
         elif domain > rang:
             len_domain = self.f.domain_bytes * 8
@@ -78,20 +73,14 @@ def hellman_preprocess(m, t, f_tag):
     :return: a list of tables, where each table is a dictionary from the end points to the start points
     """
     tables = []
-    print("In hellman_preprocess")
     for i in range(t):
-        # print(f"flavour {i=}")
         f_tag_i = lambda x: f_tag.calc((x + i) % f_tag.f.rang)
         table = defaultdict(list)
         for j in range(m):
-            # print(f"Wroking on chain {j=}")
-            # start_point = int.from_bytes(urandom(f_tag.f.domain_bytes), byteorder='big')
             start_point = j % f_tag.f.rang
             point = start_point
             for _ in range(t):
                 point = f_tag_i(point)
-            # if point in table:
-            #     print(f"Got collision in endpoint {point}, tried putting {start_point=} but has {table[point]=}")
             table[point].append(start_point)
     
         tables.append(table)
@@ -111,27 +100,23 @@ def hellman_online(tables, t, y, f_tag):
     domain = f_tag.f.domain
     rang = f_tag.f.rang
     
-    points_per_table = [y for _ in range(t)] # t as number of tables
+    # Initilize the current point for each table.
+    points_per_table = [y for _ in range(t)]
 
-    for step in range(t): # t as max number of steps
-        # print(f"In {step=}")
-        for i in range(t): # t as number of tables
-            # print(f"Working on table {i=}")
+    for step in range(t): # The maximal length of chains is t.
+        for i in range(t):
             table = tables[i]
             f_tag_i = lambda x: f_tag.calc((x + i) % f_tag.f.rang)
             current_point = f_tag_i(points_per_table[i])
             points_per_table[i] = current_point
             if current_point in table:
-                # print(f"{current_point} is endpoint")
                 for start_point in table[current_point]:
                     ptr = start_point
                     for _ in range(t):
                         if f_tag_i(ptr) == y:
-                            # print(f"Found origin")
-                            return (ptr + i) % f_tag.f.rang # flavour correction
+                            # Flavour correction, from f_tag_i to f_tag.
+                            return (ptr + i) % f_tag.f.rang
                         ptr = f_tag_i(ptr)
-                   
-    print("Didn't find origin")
     return None
 
 def run_hellman(f, m, t):
@@ -144,27 +129,16 @@ def run_hellman(f, m, t):
     """
     f_tag = ModifiedPRF(f)
 
-    print("In run_hellman !!!!!!!!!!!!!!!!")
-    print(f" before preprocess: {m=} {t=}")
     tables = hellman_preprocess(m, t, f_tag)
-    print(f" after preprocess")
 
     success_count = 0
-    counter_maybe_success = 0
     for i in range(100):
-        print("New y trial")
         y = f.calc(int.from_bytes(urandom(f.domain_bytes), byteorder='big'))
-        print(f"before hellman_online {y=}")
         x = hellman_online(tables, t, y, f_tag)
-        print(f"after hellman_online {x=}")
         if x is not None:
-            counter_maybe_success += 1
             x = f_tag.recover_x(x)
-            print(f"after recover_x, {x=}")
             if f.calc(x) == y:
-                print("success, f.calc(x)==y")
                 success_count += 1
-    print(f"{success_count=} {counter_maybe_success=}")
     return success_count
 
 
@@ -174,8 +148,6 @@ def test_1():
     block_size = 3
     m = 2 ** 8
     t = 2 ** 8
-    # m = 2 ** 14
-    # t = 2 ** 5
 
     f = PRF(key, block_size)
     return run_hellman(f, m, t)
@@ -186,12 +158,8 @@ def test_2():
     key = b'8{8H\x00\xe5\xa6\xc7BTs=\xba\xd5\x18\xe6'
     domain_size = 2
     rang_size = 3
-    # m = ?
-    # t = ?
-    # m = 2 ** 8
-    # t = 2 ** 8
-    m = 2 ** 8
-    t = 2 ** 4
+    m = 2 ** 12
+    t = 2 ** 2
 
     f = PRF(key, domain_size, rang_size)
     return run_hellman(f, m, t)
@@ -202,10 +170,8 @@ def test_3():
     key = b'\xa42A\xcf\x0c\xf4\x001\xff\xd7\xaa\x8f\tZ\x11\xdd'
     domain_size = 3
     rang_size = 2
-    # m = ?
-    # t = ?
-    m = 2 ** 8
-    t = 2 ** 8
+    m = 2 ** 16
+    t = 2 ** 4
 
     f = PRF(key, domain_size, rang_size)
     return run_hellman(f, m, t)
